be32aa162c908e2f4db11417912cacad5dd0c826
Siew Chong Che <siewchongche@gmail.com>
2021-02-10 22:36:10 +0800

    Support compile for old contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -28,11 +28,5 @@ module.exports = {
         version: "0.6.12"
       }
     },
-    // settings: {
-    //   optimizer: {
-    //     enabled: true,
-    //     runs: 200
-    //   }
-    // }
   }
 };
