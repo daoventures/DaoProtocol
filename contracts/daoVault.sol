99002e1f5fad8359df5668f03f88a35a2c33c483
Siew Chong Che <siewchongche@gmail.com>
2021-02-23 00:05:30 +0800

    Add natspec comments. Add require only EOA can withdraw and refund.

--- contracts/daoVault.sol
+++ contracts/daoVault.sol
@@ -8,6 +8,24 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
+/**
+ * New strategy contract must utilize ERC20 and with functions below:
+ * 
+ * function deposit(uint256[] memory _amounts)
+ * -> Receive list as argument
+ * -> require msg.sender == Vault
+ * 
+ * function withdraw(uint256[] memory _shares)
+ * -> Receive list as argument
+ * -> require msg.sender == Vault
+ * 
+ * function refund(uint256 _shares)
+ * -> Receive amount of shares (same amount with daoToken) as argument
+ * -> require msg.sender == Vault
+ * 
+ * function approveMigrate()
+ * -> Approve Vault to migrate all funds to new strategy
+ */
 interface IStrategy {
     function deposit(uint256[] memory _amounts) external;
     function withdraw(uint256[] memory _shares) external;
@@ -15,6 +33,7 @@ interface IStrategy {
     function balanceOf(address _address) external view returns (uint256);
 }
 
+/// @title Contract to interact between user and strategy, and distribute daoToken
 contract daoVault is ERC20, Ownable {
     using SafeERC20 for IERC20;
     using Address for address;
@@ -35,8 +54,14 @@ contract daoVault is ERC20, Ownable {
         strategy = IStrategy(_strategy);
     }
 
+    /**
+     * @notice Deposit into strategy
+     * @param _amounts A list that contains amounts to deposit
+     * Requirements:
+     * - Only EOA account can call this function
+     */
     function deposit(uint256[] memory _amounts) external {
-        require(address(msg.sender).isContract() == false, "Caller is a contract not EOA");
+        require(address(msg.sender).isContract() == false, "Only EOA");
         uint256 _before = strategy.balanceOf(address(this));
         strategy.deposit(_amounts);
         uint256 _after = strategy.balanceOf(address(this));
@@ -47,7 +72,15 @@ contract daoVault is ERC20, Ownable {
         }
     }
 
+    /**
+     * @notice Withdraw from strategy
+     * @param _shares A list that contains shares to withdraw
+     * @dev amount _shares = amount daoToken
+     * Requirements:
+     * - Only EOA account can call this function
+     */
     function withdraw(uint256[] memory _shares) external {
+        require(address(msg.sender).isContract() == false, "Only EOA");
         uint256 _before = strategy.balanceOf(address(this));
         strategy.withdraw(_shares);
         uint256 _after = strategy.balanceOf(address(this));
@@ -55,8 +88,15 @@ contract daoVault is ERC20, Ownable {
         _burn(msg.sender, _before.sub(_after));
     }
 
+    /**
+     * @notice Refund from strategy
+     * @notice This function usually only available when strategy in vesting state
+     * Requirements:
+     * - Only EOA account can call this function
+     * - Amount daoToken of user must greater than 0
+     */
     function refund() external {
-        require(address(msg.sender).isContract() == false, "Caller is a contract not EOA");
+        require(address(msg.sender).isContract() == false, "Only EOA");
         require(balanceOf(msg.sender) > 0, "No balance to refund");
 
         uint256 _shares = balanceOf(msg.sender);
@@ -66,6 +106,13 @@ contract daoVault is ERC20, Ownable {
         _burn(msg.sender, _before.sub(_after));
     }
 
+    /**
+     * @notice Set pending strategy
+     * @param _pendingStrategy Address of pending strategy
+     * Requirements:
+     * - Only owner of this contract call this function
+     * - Pending strategy must be a contract
+     */
     function setPendingStrategy(address _pendingStrategy) external onlyOwner {
         require(canSetPendingStrategy == true, "Cannot set pending strategy now");
         require(_pendingStrategy.isContract() == true, "New strategy is not contract");
@@ -73,17 +120,33 @@ contract daoVault is ERC20, Ownable {
         pendingStrategy = _pendingStrategy;
     }
 
+    /**
+     * @notice Set new strategy
+     * @dev This function only call from function migrateFunds()
+     */
     function _setStrategy() private {
         strategy = IStrategy(pendingStrategy);
         pendingStrategy = address(0);
         canSetPendingStrategy = true;
     }
 
+    /**
+     * @notice Unlock function migrateFunds()
+     * Requirements:
+     * - Only owner of this contract call this function
+     */
     function unlockMigrateFunds() external onlyOwner {
         unlockTime = block.timestamp + LOCKTIME;
         canSetPendingStrategy = false;
     }
 
+    /**
+     * @notice Migrate all funds from old strategy to new strategy
+     * Requirements:
+     * - Only owner of this contract call this function
+     * - This contract is not locked
+     * - Pending strategy is set
+     */
     function migrateFunds() external onlyOwner {
         require(unlockTime <= block.timestamp && unlockTime + 1 days >= block.timestamp, "Function locked");
         require(token.balanceOf(address(strategy)) > 0, "No balance to migrate");
@@ -97,6 +160,6 @@ contract daoVault is ERC20, Ownable {
         oldStrategyToken.safeTransfer(address(strategy), oldStrategyToken.balanceOf(address(this)));
 
         _setStrategy();
-        unlockTime = 0;
+        unlockTime = 0; // Lock back this function
     }
 }
